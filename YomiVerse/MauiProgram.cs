6b3beccb7720e17d3a62235362116cf00c087fa8
IasysVivekMaurya <vivek.maurya@iasys.co.in>
2025-07-10 15:18:50 +0530

    Changes for App Icon and Splash screen

--- YomiVerse/MauiProgram.cs
+++ YomiVerse/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Toolkit.Hosting;
 
 namespace YomiVerse
 {
@@ -9,6 +11,8 @@ namespace YomiVerse
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureSyncfusionToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
---
+++ YomiVerse/Views/LandingPageView.xaml.cs
@@ -0,0 +1,9 @@
+namespace YomiVerse.Views;
+
+public partial class LandingPageView : ContentPage
+{
+	public LandingPageView()
+	{
+		InitializeComponent();
+	}
+}
